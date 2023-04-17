8eff3e55cff405acdd4400bf7af56c0525b6d6b0
Макс <maxim__90.90@mail.ru>
2023-04-17 15:33:04 +0400

    FirstCommit

---
+++ src/main/java/com/example/exception/ExceptionController.java
@@ -0,0 +1,7 @@
+package com.example.exception;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExceptionController {
---
+++ src/main/java/com/example/exception/ExceptionServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.exception;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExceptionServiceImpl implements ExceptionService {
+
+}
