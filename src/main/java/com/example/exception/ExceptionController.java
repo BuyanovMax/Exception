196c4561600e495a70b4ad16dd515df21ba3c19d
Макс <maxim__90.90@mail.ru>
2023-04-19 09:53:49 +0400

    AllIsReady

--- src/main/java/com/example/exception/ExceptionController.java
+++ src/main/java/com/example/exception/ExceptionController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.regex.PatternSyntaxException;
+
+
 @RestController
 public class ExceptionController {
     private static ExceptionService exceptionService;
@@ -23,6 +26,7 @@ public class ExceptionController {
         } catch (WrongPasswordException e) {
             return false;
         }
+
 
     }
 
